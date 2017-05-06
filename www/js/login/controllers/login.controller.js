a3488cc1b183e3131772aff31286bd5ebc46fc43
Denner Evaldt <dnrevaldt@gmail.com>
2017-05-05 21:49:41 -0300

    progress confirmation

--- www/js/login/controllers/login.controller.js
+++ www/js/login/controllers/login.controller.js
@@ -5,16 +5,30 @@
         .module('app.login')
         .controller('LoginController', LoginController);
 
-    LoginController.$inject = ['LoginService', '$state', '$ionicLoading', '$timeout', '$cordovaToast', '$cordovaOauth'];
+    LoginController.$inject = ['LoginService', '$state', '$ionicLoading', '$timeout', '$cordovaToast', '$cordovaOauth', '$ionicModal', '$scope', 'PlaceApiService'];
 
     /* @ngInject */
-    function LoginController(LoginService, $state, $ionicLoading, $timeout, $cordovaToast, $cordovaOauth) {
+    function LoginController(LoginService, $state, $ionicLoading, $timeout, $cordovaToast, $cordovaOauth, $ionicModal, $scope, PlaceApiService) {
         var vm = this;
         vm.user = {};
         vm.userCreate = {};
         vm.submitLogin = submitLogin;
         vm.loginFacebook = loginFacebook;
         vm.userLogged = {};
+        vm.userConfirm = {};
+        vm.createAccount = createAccount;
+        vm.search = search;
+        vm.listCities = [];
+        vm.setCitie = setCitie;
+
+        ///////////////
+
+        // modal options
+        $ionicModal.fromTemplateUrl('templates/confirmAccount.html', {
+          scope: $scope
+        }).then(function(modal) {
+          vm.modal = modal;
+        });
 
         //////////////
 
@@ -50,8 +64,13 @@
         function loginFacebook() {
           $cordovaOauth.facebook("545567282282477", ["public_profile", "email"]).then(function(result) {
             var access_token = result.access_token;
+            var result_fb;
             loadOn();
             LoginService.getDataFB(access_token)
+              .then(function(response){
+                result_fb = response;
+                return response;
+              })
               .then(LoginService.tokenFacebook)
               .then(LoginService.setCredentials)
               .then(LoginService.getUserData)
@@ -68,15 +87,87 @@
 
                 $ionicLoading.hide();
               }, function (err) {
+                vm.userConfirm.typeaccount = '1';
+                vm.userConfirm.fullname = result_fb.data.name;
+                vm.userConfirm.email = result_fb.data.email;
+                vm.userConfirm.password = result_fb.data.id;
+                //
                 $ionicLoading.hide();
-                $cordovaToast
-                  .show('Erro ao buscar credenciais, tente novamente', 'long', 'center');
+                vm.modal.show();
               });
 
           }, function(error) {
             if(error !== "The sign in flow was canceled")
               $cordovaToast.show('Usuário não encontrado, verifique e tente novamente', 'long', 'center');
           });
+        }
+
+        function search() {
+          PlaceApiService.getCitiesAutoComplete(vm.userConfirm.search)
+            .then(function (response) {
+              vm.listCities = response;
+            }, function (error) {
+              $cordovaToast
+                .show('Erro ao buscar cidades', 'long', 'center');
+            });
+        }
+
+        function setCitie(item) {
+          vm.userConfirm.search = item.description;
+          vm.listCities = [];
+          PlaceApiService.getLatLngCitie(item.place_id)
+            .then(function (response) {
+              vm.userConfirm.lat = response.lat;
+              vm.userConfirm.lng = response.lng;
+            });
+        }
+
+        function createAccount() {
+          if (vm.form.$invalid) {
+            $cordovaToast
+              .show('Preencha os campos corretamente', 'long', 'center');
+            return;
+          }
+          loadOn();
+          if (vm.userConfirm.typeaccount === '1') {
+            // player
+            LoginService.createAccountPlayer(vm.userConfirm)
+            .then(function(response){
+              return LoginService.token(vm.userConfirm.username, vm.userConfirm.password);
+            })
+            .then(LoginService.setCredentials)
+            .then(LoginService.getUserData)
+            .then(LoginService.setUserData)
+            .then(function(response) {
+              // redirect player dash
+              vm.modal.hide();
+              $state.go('playerController.jogos');
+              $ionicLoading.hide();
+            }, function(err){
+              $ionicLoading.hide();
+              $cordovaToast
+                .show(err.data.error[0].message || 'Confira todos os campos', 'long', 'center');
+            });
+          } else {
+            // enterprise
+            LoginService.createAccountEnterprise(vm.userConfirm)
+              .then(function(response){
+                return LoginService.token(vm.userConfirm.username, vm.userConfirm.password);
+              })
+              .then(LoginService.setCredentials)
+              .then(LoginService.getUserData)
+              .then(LoginService.setUserData)
+              .then(function(response) {
+                // redirect enterprise dash
+                vm.modal.hide();
+                $state.go('enterpriseController.quadras');
+                $ionicLoading.hide();
+              }, function(err){
+                $ionicLoading.hide();
+                $cordovaToast
+                  .show(err.data.error[0].message || 'Confira todos os campos', 'long', 'center');
+              });
+          }
         }
 
         function loadOn() {
